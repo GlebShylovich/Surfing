b976f40f541ae7e56c9fa7cb03eb046d5145e9f5
GlebShylovich <glebshylovich10@gmail.com>
2024-06-18 18:53:07 +0300

    redux toolkit

---
+++ src/Services/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from '@reduxjs/toolkit'
+import { setupListeners } from "@reduxjs/toolkit/query/react";
+
+export const store = configureStore({
+    reducer: {},
+})
+
+setupListeners(store.dispatch);
