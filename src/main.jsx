52c0f94f8d35460f1ae81041ef0f1a0010502c90
GlebShylovich <glebshylovich10@gmail.com>
2024-06-19 12:53:19 +0300

    store fix

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,18 @@ import App from './App/App.jsx'
 import { initializeApp } from "firebase/app";
 import appConfig from '../config.js'
 import { BrowserRouter } from 'react-router-dom';
+import { store } from './Services/store.js'
+import { Provider } from 'react-redux'
 import './Styles/index.scss'
 
 initializeApp(appConfig)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
 )
