8d0de49679fba0839131dd4e86ea1d86110c4ebb
GlebShylovich <glebshylovich10@gmail.com>
2024-06-21 14:45:59 +0300

    database & services

--- config.js
+++ config.js
@@ -5,5 +5,6 @@ export default {
     storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
     appId: import.meta.env.VITE_FIREBASE_APP_ID,
-    measurementId: import.meta.env.VITE_MEASUREMENT_ID
+    measurementId: import.meta.env.VITE_MEASUREMENT_ID,
+    databaseURL: import.meta.env.VITE_DATABASE_URL
   };
