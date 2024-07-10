52b9af578381e348276d233dca6deb7b63386a66
GlebShylovich <glebshylovich10@gmail.com>
2024-07-10 14:07:27 +0200

    auth bug fix

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,23 @@
-import { Link } from "react-router-dom";
-import "./Home.jsx";
+import { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { getAuth } from "firebase/auth";
+import "./Home.scss";
+
 export default function Home() {
+  const navigate = useNavigate();
+  const auth = getAuth();
+
+  useEffect(() => {
+    if (auth.currentUser) {
+      navigate("/");
+    } else {
+      navigate("/auth");
+    }
+  }, [auth.currentUser]);
+
   return (
-    <>
-      <Link to='/profile'>Account</Link>
-    </>
+    <div className="home">
+      <Link to="/profile">Account</Link>
+    </div>
   );
 }
